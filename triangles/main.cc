efb3b768ca08fd8c7f6ea32bf8c88c2e862a428b
akolechko <kolechkoandrey@gmail.com>
2020-11-29 00:21:48 +0200

    15 hours of tRiAnGlEs

--- triangles/main.cc
+++ triangles/main.cc
@@ -1,15 +1,72 @@
 #include <iostream>
+#include <locale>
 #include "shape_handler.h"
+#include "validation.h"
+#include "ui.h"
+
+void ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+          }));
+}
+
+void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+                       [](unsigned char ch) { return !std::isspace(ch); })
+              .base(),
+          s.end());
+}
+
+void trim(std::string &s) {
+  ltrim(s);
+  rtrim(s);
+}
+
+std::vector<std::string> StringSplitter(const std::string& str) {
+  std::vector<std::string> v;
+  std::string s = str;
+  size_t position = 0;
+  std::string substring;
+
+  while ((position = s.find(",")) != std::string::npos) {
+    substring = s.substr(0, position);
+    trim(substring);
+    v.push_back(substring);
+    s.erase(0, position + 1);
+  }
+  trim(substring);
+  v.push_back(substring);
+
+  return v;
+}
 
 int main() {
   ShapeHandler<Triangle> shandler;
+  UI ui;
+  std::string str;
+  std::vector<std::string> vs;
+  std::vector<double> vd;
+  bool flag = true;
 
-  Triangle triangle1("Kev", 2, 2, 3);
-  Triangle triangle2("Kevi", 5, 5, 9);
-  Triangle triangle3("Kevin", 4, 2, 5);
+  while (flag) {
+    ui.RequestData(str, "[name], [a], [b], [c]: ");
+    vs = StringSplitter(str);
 
-  shandler.AddShape(triangle3);
-  shandler.AddShape(triangle2);
+    if (vs.size() == 4) {
+      flag = false;
+
+      for (int i = 1; i < vs.size(); ++i) {
+        if (IsValid<double>(vs[i]))
+          vd.push_back(strtod(vs[i].c_str(), nullptr));
+        else
+          flag = true;
+      }
+    }
+    if (flag) std::cout << "Invalid input. Try again." << std::endl;
+  }
+
+  Triangle triangle1(vs[0], vd[0], vd[1], vd[2]);
+
   shandler.AddShape(triangle1);
 
   std::vector<Triangle> vec = shandler.GetSortedShapes();
---
+++ triangles/validation.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <string>
+#include "shape.h"
+
+template <class T>
+bool IsValid(const std::string &str) = delete;
+
+template <>
+bool IsValid<long>(const std::string &str) {
+  errno = 0;
+  char *endptr;
+
+  (void)strtol(str.c_str(), &endptr, 0);
+
+  if (errno != 0) {
+    errno = 0;
+    return 0;
+  }
+
+  if (endptr == str || *endptr != '\0') return 0;
+
+  return 1;
+}
+
+template <>
+bool IsValid<double>(const std::string &str) {
+  errno = 0;
+  char *endptr;
+
+  (void)strtod(str.c_str(), &endptr);
+
+  if (errno != 0) {
+    errno = 0;
+    return 0;
+  }
+
+  if (endptr == str || *endptr != '\0') return 0;
+
+  return 1;
+}
