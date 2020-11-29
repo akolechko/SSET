55eeb6e66a9249aa8714b76679eb67bb21de3ec2
akolechko <kolechkoandrey@gmail.com>
2020-11-29 11:10:42 +0200

    Triangles: Working version

--- triangles/main.cc
+++ triangles/main.cc
@@ -1,76 +1,60 @@
 #include <iostream>
 #include <locale>
 #include "shape_handler.h"
+#include "string_handlers.h"
 #include "validation.h"
 #include "ui.h"
-
-void ltrim(std::string &s) {
-  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-            return !std::isspace(ch);
-          }));
-}
-
-void rtrim(std::string &s) {
-  s.erase(std::find_if(s.rbegin(), s.rend(),
-                       [](unsigned char ch) { return !std::isspace(ch); })
-              .base(),
-          s.end());
-}
-
-void trim(std::string &s) {
-  ltrim(s);
-  rtrim(s);
-}
-
-std::vector<std::string> StringSplitter(const std::string& str) {
-  std::vector<std::string> v;
-  std::string s = str;
-  size_t position = 0;
-  std::string substring;
-
-  while ((position = s.find(",")) != std::string::npos) {
-    substring = s.substr(0, position);
-    trim(substring);
-    v.push_back(substring);
-    s.erase(0, position + 1);
-  }
-  trim(substring);
-  v.push_back(substring);
-
-  return v;
-}
 
 int main() {
   ShapeHandler<Triangle> shandler;
   UI ui;
   std::string str;
-  std::vector<std::string> vs;
-  std::vector<double> vd;
+  std::string delimiter = ",";
+  std::vector<std::string> substrings;
+  std::vector<double> sides;
   bool flag = true;
 
   while (flag) {
     ui.RequestData(str, "[name], [a], [b], [c]: ");
-    vs = StringSplitter(str);
 
-    if (vs.size() == 4) {
+    if ((substrings = StringSplitter(str, delimiter)).size() == 4) {
       flag = false;
 
-      for (int i = 1; i < vs.size(); ++i) {
-        if (IsValid<double>(vs[i]))
-          vd.push_back(strtod(vs[i].c_str(), nullptr));
+      for (int i = 1; i < substrings.size(); ++i) {
+        if (IsValid<double>(substrings[i]))
+          sides.push_back(strtod(substrings[i].c_str(), nullptr));
         else
           flag = true;
       }
     }
-    if (flag) std::cout << "Invalid input. Try again." << std::endl;
+
+    if (flag) {
+      ui.SendMessage("Invalid input. Try again.");
+      substrings.clear();
+      sides.clear();
+    }
+    else {
+      Triangle triangle(substrings[0], sides[0], sides[1], sides[2]);
+      shandler.AddShape(triangle);
+
+      substrings.clear();
+      sides.clear();
+
+      std::string answer;
+      ui.RequestData(answer, "Do you want to add one more shape? (y/n): ");
+      answer = ToLower(answer);
+      if (answer == "y" || answer == "yes") flag = true;
+    }
   }
 
-  Triangle triangle1(vs[0], vd[0], vd[1], vd[2]);
 
-  shandler.AddShape(triangle1);
+
 
   std::vector<Triangle> vec = shandler.GetSortedShapes();
-  for (Triangle t : vec) std::cout << t.GetArea() << std::endl;
+  ui.SendMessage("============= Triangles list: ===============");
+  for (int i = 0; i < vec.size(); ++i)
+    std::cout << i + 1 << ". [Triangle " << vec[i].GetName()
+              << "]: " << vec[i].GetArea() << " cm" << std::endl;
 
   return 0;
 }
---
+++ triangles/string_handlers.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <string>
+#include <vector>
+
+void LTrim(std::string &str) {
+  str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+          }));
+}
+
+void RTrim(std::string &str) {
+  str.erase(std::find_if(str.rbegin(), str.rend(),
+                       [](unsigned char ch) { return !std::isspace(ch); })
+              .base(),
+          str.end());
+}
+
+void Trim(std::string &str) {
+  LTrim(str);
+  RTrim(str);
+}
+
+std::vector<std::string> StringSplitter(const std::string& str,
+    std::string delimiter) {
+  std::vector<std::string> substrings;
+  std::string s = str;
+  std::string substring;
+  size_t position = 0;
+
+  while ((position = s.find(delimiter)) != std::string::npos) {
+    substring = s.substr(0, position);
+    Trim(substring);
+    substrings.push_back(substring);
+    s.erase(0, position + delimiter.length());
+  }
+  Trim(s);
+  substrings.push_back(s);
+
+  return substrings;
+}
+
+std::string ToLower(const std::string &str) {
+  std::string s;
+  for (char ch : str) s += tolower(ch);
+  
+  return s;
+}
